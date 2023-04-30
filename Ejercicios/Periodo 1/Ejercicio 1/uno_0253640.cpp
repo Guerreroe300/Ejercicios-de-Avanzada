e49b5296afc6f30452f5d2e7b0560b1259e4ad61
Alex <guerreroe200@yahoo.com>
2023-04-30 17:32:52 -0600

    Ejercicio 1.1

---
+++ Ejercicios/Periodo 1/Ejercicio 1/uno_0253640.cpp
@@ -0,0 +1,11 @@
+/*
+Esta es la estructura general de un programa en c++
+0253640 Alejandro Guerrero Alvarez
+*/
+
+#include <iostream>
+
+int main(int args, const char* argv[]){
+    std::cout << "Hola, curso c++" << std::endl;
+    return 0;
+}
