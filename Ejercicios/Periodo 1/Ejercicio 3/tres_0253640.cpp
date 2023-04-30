30360648997ff9f62604d8544a7077d062f70dbd
Alejandro Guerrero <guerreroe200@yahoo.com>
2023-04-30 17:57:44 -0600

    Ejercicio 1.3

---
+++ Ejercicios/Periodo 1/Ejercicio 3/tres_0253640.cpp
@@ -0,0 +1,15 @@
+/*
+Aqui se esnseñan identificadores que se pueden usar y en comentarios los que no
+0253640 Alejandro Guerrero Alvarez
+*/
+
+#include <iostream>
+
+int main(int args, const char* argv[]){
+    //int int;
+    //int 45numero;
+    int numero42;
+    int altura;
+    char _charNombre;
+    return 0;
+}
