dd45c3e912d1f9dbad4780905c0c6f82127990ab
Alejandro Guerrero <guerreroe200@yahoo.com>
2023-04-30 22:00:14 -0600

    ejericio 1.17

---
+++ Ejercicios/Periodo 1/Ejercicio 17/diecisiete_0253640.cpp
@@ -0,0 +1,14 @@
+/*
+Ejemplo de un metodo
+0253640 Alejandro Guerrero Alvarez
+*/
+
+#include <iostream>
+
+void decirHola(){
+    std::cout << "Hola, buenas tardes\n";
+}
+
+int main(int args, const char* argv[]){
+    decirHola();
+}
