c17993bb248897f2e309519567091821e75378c5
Alejandro Guerrero <guerreroe200@yahoo.com>
2023-04-30 20:31:37 -0600

    Ejercicio 1.7

---
+++ Ejercicios/Periodo 1/Ejercicio 7/siete_0253640.cpp
@@ -0,0 +1,12 @@
+/*
+Aqui se un ejemplo de un operador bit
+0253640 Alejandro Guerrero Alvarez
+*/
+
+#include <iostream>
+
+int main(int args, const char* argv[]){
+    char a = 1;
+
+    std::cout << (a<<4) << std::endl;
+}
